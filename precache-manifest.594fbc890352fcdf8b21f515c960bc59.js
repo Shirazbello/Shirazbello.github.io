b48b54b8834eeec119c74fcb5366df258e73b66f
Shirazbello <shirazbello@gmail.com>
2022-08-23 17:36:27 +0300

    Updates

---
+++ precache-manifest.594fbc890352fcdf8b21f515c960bc59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108e0e84173f91c79dba39a71a7db05a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d3c8c48112c38c23f05",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3c8c48112c38c23f05",
+    "url": "/static/js/main.6676e89a.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "ff3510f834417d21b5c639867d2cf683",
+    "url": "/static/media/Shiraz.ff3510f8.jpg"
+  },
+  {
+    "revision": "9ea4b67f9b89d9fe79daa1453db28bee",
+    "url": "/static/media/resume.9ea4b67f.pdf"
+  }
+]);
